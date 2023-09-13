58d51a86f992e783c4f9ad3029951af25a6da05d
Samuel Chung <41113285+schungzhihuai@users.noreply.github.com>
2023-09-13 12:30:18 +0800

    Update app.py
    added webhook route to receive data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/webhook',methods = ["POST"])
+def getReading():
+    # Convert the request data to JSON (assuming it's sent as JSON)
+    data = request.json
+    print("Received data:", data)
+
+    return jsonify({"message": "Data received successfully!"}),200
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=6969, debug=True)
