0e80a338514b7a76792f2c842d09582fbe6f65b8
Samuel Chung <41113285+schungzhihuai@users.noreply.github.com>
2023-09-15 22:33:51 +0800

    Update for flask, phone html, ngrok script
    1. Created phone html to post audio
    2. Update flask to pass route to html and parse audio correctly
    3. Created script to handle ngrok
    4. Created .env to host PORT and ngrok variable
    5. update requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
 # app.py
 
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+import os
+import requests
 
 app = Flask(__name__)
 
+load_dotenv()
+NGROK_URL=os.getenv('NGROK_URL')
+PORT=os.getenv('PORT')
+
+
+def getUrl():
+    return requests.get(NGROK_URL).json()['tunnels'][0]['public_url']
+    
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',ngrok_url = getUrl())
+
+@app.route('/phone')
+def phone():
+    return render_template('phone.html',ngrok_url = getUrl())
+
+@app.route('/audio', methods=['POST'])
+def receive_audio():
+    # Check if the post request has the file part
+    if 'audio' not in request.files:
+        return 'No file part', 400
+
+    audio_file = request.files['audio']
+    
+    # If the user does not select a file, the browser might
+    # submit an empty file without a filename.
+    if audio_file.filename == '':
+        return 'No selected file', 400
+
+    if audio_file:
+        # Save or process the audio file as needed
+        # For example, to save the file:
+        filepath = "path_where_you_want_to_save_the_file.wav"
+        audio_file.save(filepath)
+
+        return 'Audio received and saved', 200
+
+    return 'Unknown error', 500
 
 @app.route('/webhook',methods = ["POST"])
 def getReading():
@@ -17,4 +56,4 @@ def getReading():
     return jsonify({"message": "Data received successfully!"}),200
 
 if __name__ == '__main__':
-    app.run(port=6969, debug=True)
+    app.run(port=PORT, debug=True)
